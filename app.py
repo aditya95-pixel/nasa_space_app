821d3160e1100e195104df267d3614d470b9cf97
Aditya Mazumdar <136506411+aditya95-pixel@users.noreply.github.com>
2024-12-28 15:12:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# load packages==============================================================
 from flask import Flask, render_template, request, redirect, url_for, session, flash,jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
